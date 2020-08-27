4df838bd7e6186585433602fbcf37c65bc5807f2
jbobd <yarboclos@MacBook-Pro-de-juan.local>
2020-08-27 19:36:14 -0300

    boilerplate listo (?

---
+++ client/src/pages/landing/Landing.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from "@chakra-ui/core";
+
+const Landing = () => {
+    return(
+        <Button>Soy un boton de Chakra</Button>
+    )
+};
+
+export default Landing;
