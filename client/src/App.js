813b343ba17a7adc9fc2523268cffd11ac9fdb3d
Franco Tegaldi <tegaldifranco@yahoo.com.ar>
2020-08-27 19:49:18 -0300

    added routes and login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,32 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 
+/* pages */
 import Landing from "./pages/landing/Landing";
+import LoginPage from "./pages/Login/Login";
 
 
 const App = () => {
   return (
     <ThemeProvider>
+      <Router>
+        <nav>SOY UN NAVBAR Y NO SOY DE CHAKRA</nav>
       <CSSReset />
-      <Landing />
+      <Switch>
+        <Route path='login'>
+          <LoginPage />
+        </Route>
+        <Route path='/home'>
+          <Landing /> 
+        </Route>
+      </Switch>
+      </Router>
     </ThemeProvider>
   );
 }
---
+++ client/src/pages/Login/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginPage = () => {
+    return (
+        <button>asdsdas</button>
+    )
+}
+
+export default LoginPage
